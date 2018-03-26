064d43c53902af8b9fec153dc872317283c95db4
GLEYVER COUTINHO CASTRO <16254851@CFP127ALUNO.LOCAL>
2018-03-26 11:06:24 -0300

    'glash'

---
+++ views/sobre_hhealth.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <!-- Link para estilização da pagina -->
+    <link rel="stylesheet" type="text/css" href="../css/style_nav.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_footer.css">
+    <link rel="stylesheet" type="text/css" href="../css/style_sobre_hhealth.css">
+
+    <meta charset="utf-8">
+    <title>Sobre Hhealth</title>
+  </head>
+  <body>
+    <!-- Div que segura todas o conteuddo da página -->
+     <div id="main">
+      <!-- incluindo página menu da página -->
+      <?php //require_once('nav.php'); ?>
+
+      <!-- Div que segura todo conteúdo da página -->
+      <div id="content_sobre_hhealth">
+        <!-- titulo da página -->
+        <div id="suporte_titulo">
+          <div id="titulo_pagina">
+            <strong>Sobre o Hhealth</strong>
+          </div>
+        </div>
+        <!-- Faixa branca embaixo do menu -->
+        <div class="faixa_branca">
+
+        </div>
+
+        <!-- Primeiro texto da página -->
+        <div id="suporte_primeiro_texto">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+        </div>
+
+        <!-- Primeiro imagem  com texto da página -->
+        <div class="suporte_texto_imagem">
+          <div class="imagens_sobre_hhealth">
+            <figure class="suporte_imagem">
+              <img src="../imagens/imgFaleConosco.png" alt="" class="imagem_representa">
+            </figure>
+          </div>
+          <div class="texto_sobre_hhealth">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+          </div>
+        </div>
+
+        <!-- Segunda imagem  com texto da página -->
+        <div class="suporte_texto_imagem">
+          <div class="texto_sobre_hhealth">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+          </div>
+          <div class="imagens_sobre_hhealth">
+            <figure class="suporte_imagem">
+              <img src="../imagens/imgFaleConosco.png" alt="" class="imagem_representa">
+            </figure>
+          </div>
+        </div>
+
+        <!-- Terceira imagem  com texto da página -->
+        <div class="suporte_texto_imagem">
+          <div class="imagens_sobre_hhealth">
+            <figure class="suporte_imagem">
+              <img src="../imagens/imgFaleConosco.png" alt="" class="imagem_representa">
+            </figure>
+          </div>
+          <div class="texto_sobre_hhealth">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.
+          </div>
+        </div>
+
+
+      </div>
+      <!-- incluindo rodapé da pagina -->
+      <?php include_once('footer.php'); ?>
+    </div>
+  </body>
+</html>
---
+++ views/unidades_hhealth.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!DOCTYPE html>
+    <html lang="pt-br" dir="ltr">
+      <head>
+        <!-- Link para estilização da pagina -->
+        <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="../css/style_nav.css">
+        <link rel="stylesheet" type="text/css" href="../css/style_footer.css">
+        <link rel="stylesheet" type="text/css" href="../css/style_unidade.css">
+
+        <meta charset="utf-8">
+        <title>Unidades</title>
+      </head>
+      <body>
+        <!-- Div que segura todas o conteuddo da página -->
+         <div id="main">
+          <!-- incluindo página menu da página -->
+          <?php //require_once('nav.php'); ?>
+
+          <!-- Div que segura todo conteúdo da página -->
+          <div id="content_sobre_hhealth">
+            <!-- titulo da página -->
+            <div id="suporte_titulo">
+              <div id="titulo_pagina">
+                <strong>Unidades</strong>
+              </div>
+            </div>
+            <!-- Faixa branca embaixo do menu -->
+            <div class="faixa_branca">
+
+            </div>
+
+            <!-- Segura a barra de pesquisa de unidade -->
+            <div id="suporte_barra_pesquisa">
+
+              <input type="text" name="txt_barra_pesquisa" value="" placeholder="Search" id="input_pesquisa"><!-- Barra de pesquisa da unidade-->
+              <div id="lupa" onclick="alert('ok :)');"></div>
+            </div>
+
+            <!-- Div que segura as unidades -->
+            <div class="suporte_unidade">
+              <!-- Div que segura titulo -->
+              <div class="suporte_titulo_item_unidade">
+                <div class="titulo_item"> <!-- Titulo -->
+                  Unidade 1
+                </div>
+              </div>
+
+              <!-- div imagem -->
+              <div class="imagem_unidade">
+
+              </div>
+
+              <!-- Mostra localização da unidade -->
+              <div class="mapa_unidade">
+
+              </div>
+            </div>
+
+            <!-- Div que segura as unidades -->
+            <div class="suporte_unidade">
+              <!-- Div que segura titulo -->
+              <div class="suporte_titulo_item_unidade">
+                <div class="titulo_item"> <!-- Titulo -->
+                  Unidade 1
+                </div>
+              </div>
+
+              <!-- div imagem -->
+              <div class="imagem_unidade">
+
+              </div>
+
+              <!-- Mostra localização da unidade -->
+              <div class="mapa_unidade">
+
+              </div>
+            </div>
+
+            <!-- Div que segura as unidades -->
+            <div class="suporte_unidade">
+              <!-- Div que segura titulo -->
+              <div class="suporte_titulo_item_unidade">
+                <div class="titulo_item"> <!-- Titulo -->
+                  Unidade 1
+                </div>
+              </div>
+
+              <!-- div imagem -->
+              <div class="imagem_unidade">
+
+              </div>
+
+              <!-- Mostra localização da unidade -->
+              <div class="mapa_unidade">
+
+              </div>
+            </div>
+          </div>
+          <!-- incluindo rodapé da pagina -->
+          <?php include_once('footer.php'); ?>
+        </div>
+      </body>
+    </html>
+
+  </body>
+</html>
