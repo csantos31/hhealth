1e2777dc437922373bba7a7e9e928086b1172f6f
GLEYVER COUTINHO CASTRO <16254851@CFP127ALUNO.LOCAL>
2018-03-27 07:59:38 -0300

    gleyver home

--- views/nav.php
+++ views/nav.php
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-        <div class="suporte_menu">
-          <div class="menu"><!--**MENU**-->
-            <div class="header"><!--header-->
-                  <a href="index.php">
-                        <div class="logo">
-                        </div>
-                  </a>
-                <nav class="nav_menu">
-                      <ul class="menu_nav">
-=======
+
+
       <div class="header"><!--header-->
             <a href="index.php">
                   <div class="logo">
@@ -36,47 +27,32 @@
                               </a>
                         </ul>
                     </li>
-                    <a href="views/dicas_saude.php">
->>>>>>> 343a5d963bf1b3bf92594cbf66edbc2abe96f778
-                          <li class="item_nav">
-                              Hospital
-                              <ul class="submenu_nav_hhealth">
-                                    <a href="home.php">
-                                          <li class="item_submenu_nav_hhealth">Home</li>
-                                    </a>
-                                    <li class="item_submenu_nav_hhealth">Especialidades</li>
-                                    <li class="item_submenu_nav_hhealth">Ambientes</li>
-                                    <li class="item_submenu_nav_hhealth">Sobre</li>
-                              </ul>
-                          </li>
-                          <a href="views/dicas_saude.php">
-                                <li class="item_nav">
-                                    Dicas de Saúde
-                                </li>
-                          </a>
-                          <li class="item_nav">
-                              Unidades
-                          </li>
+                      <a href="views/dicas_saude.php">
+                            <li class="item_nav">
+                                Dicas de Saúde
+                            </li>
+                      </a>
+                      <li class="item_nav">
+                          Unidades
+                      </li>
 
-                          <li class="item_nav">
-                              Exames
-                          </li>
+                      <li class="item_nav">
+                          Exames
+                      </li>
 
-                          <li class="item_nav">
-                              Convênios
-                          </li>
+                      <li class="item_nav">
+                          Convênios
+                      </li>
 
-                          <li class="item_nav">
-                              Contatos
-                              <ul class="submenu_nav_contatos">
-                                    <a href="views/fale_conosco.php">
-                                          <li class="item_submenu_nav_contatos">Fale Conosco</li>
-                                    </a>
-                                    <li class="item_submenu_nav_contatos">Trabalhe Conosco</li>
-                              </ul>
-                          </li>
-                      </ul>
-                </nav>
-            </div>
+                      <li class="item_nav">
+                          Contatos
+                          <ul class="submenu_nav_contatos">
+                                <a href="views/fale_conosco.php">
+                                      <li class="item_submenu_nav_contatos">Fale Conosco</li>
+                                </a>
+                                <li class="item_submenu_nav_contatos">Trabalhe Conosco</li>
+                          </ul>
+                      </li>
+                  </ul>
+            </nav>
         </div>
-      </div>
