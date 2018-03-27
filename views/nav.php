060782cb3ee5e12997cf09c1f3196e04ce64fc1b
gleyver <gleyvercc@outlook.com>
2018-03-27 20:56:35 -0300

    commit 27-03-2018

--- views/nav.php
+++ views/nav.php
@@ -1,52 +1,56 @@
+<div class="header"><!--header-->
+      <a href="index.php">
+            <div class="logo">
+            </div>
+      </a>
+    <nav class="nav_menu">
+          <div class="faixa_texte">
 
-
-        <div class="suporte_menu">
-          <div class="menu"><!--**MENU**-->
-            <div class="header"><!--header-->
-                  <a href="index.php">
-                        <div class="logo">
-                        </div>
-                  </a>
-                <nav class="nav_menu">
-                      <ul class="menu_nav">
-      <a href="views/dicas_saude.php">
-
-                          <li class="item_nav">
-                              Hospital
-                              <ul class="submenu_nav_hhealth">
-                                    <a href="home.php">
-                                          <li class="item_submenu_nav_hhealth">Home</li>
-                                    </a>
-                                    <li class="item_submenu_nav_hhealth">Especialidades</li>
-                                    <li class="item_submenu_nav_hhealth">Ambientes</li>
-                                    <li class="item_submenu_nav_hhealth">Sobre</li>
-                              </ul>
-                          </li>
-                          <a href="views/dicas_saude.php">
-                                <li class="item_nav">
-                                    Dicas de Saúde
-                                </li>
-                          </a>
-                          <li class="item_nav">
-                              Unidades
-
-                      <li class="item_nav">
-                          Exames
-                      </li>
-
-                      <li class="item_nav">
-                          Convênios
-                      </li>
-
-                      <li class="item_nav">
-                          Contatos
-                          <ul class="submenu_nav_contatos">
-                                <a href="views/fale_conosco.php">
-                                      <li class="item_submenu_nav_contatos">Fale Conosco</li>
-                                </a>
-                                <li class="item_submenu_nav_contatos">Trabalhe Conosco</li>
-                          </ul>
-                      </li>
+          </div>
+          <ul class="menu_nav">
+              <li class="item_nav">
+                  Hospital
+                  <ul class="submenu_nav_hhealth">
+                        <a href="../index.php">
+                              <li class="item_submenu_nav_hhealth">Home</li>
+                        </a>
+                        <a href="views/especialidades.php">
+                              <li class="item_submenu_nav_hhealth">Especialidades</li>
+                        </a>
+                        <a href="ambientes.php">
+                              <li class="item_submenu_nav_hhealth">Ambientes</li>
+                        </a>
+                        <a href="sobre_hhealth.php">
+                              <li class="item_submenu_nav_hhealth">Sobre</li>
+                        </a>
                   </ul>
-            </nav>
-        </div>
+              </li>
+              <a href="views/dicas_saude.php">
+                    <li class="item_nav">
+                        Dicas de Saúde
+                    </li>
+              </a>
+              <li class="item_nav">
+                  Unidades
+              </li>
+
+              <li class="item_nav">
+                  Exames
+              </li>
+
+              <li class="item_nav">
+                  Convênios
+              </li>
+
+              <li class="item_nav">
+                  Contatos
+                  <ul class="submenu_nav_contatos">
+                        <a href="views/fale_conosco.php">
+                              <li class="item_submenu_nav_contatos">Fale Conosco</li>
+                        </a>
+                        <li class="item_submenu_nav_contatos">Trabalhe Conosco</li>
+                  </ul>
+              </li>
+          </ul>
+    </nav>
+</div>
