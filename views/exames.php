c628c858e9eff264375b6ece7d96fd8d39bc4591
wesley49 <wesley.wesley.bezerra49@hotmail.com>
2018-03-27 22:41:48 -0300

    tela de exames

--- views/exames.php
+++ views/exames.php
@@ -47,14 +47,123 @@
                             </div>
 
                             <div class="acoes_exames"><!--ação 1-->
-                                <img src="">
+                                <img src="../imagens/info.png">
                             </div>
 
                             <div class="acoes_exames"><!--ação 2-->
-                                <img src="">
+                                <img src="../imagens/user.png">
+                            </div>
+                        </div>
+                        
+                        <div class="content_faixa_exames"><!--content dos exames-->
+                            <div class="content_nome_exames"><!--nome exames-->
+                                <a>Cardiologia</a>
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 1-->
+                                <img src="../imagens/info.png">
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 2-->
+                                <img src="../imagens/user.png">
+                            </div>
+                        </div>
+                        
+                        <div class="content_faixa_exames"><!--content dos exames-->
+                            <div class="content_nome_exames"><!--nome exames-->
+                                <a>Cardiologia</a>
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 1-->
+                                <img src="../imagens/info.png">
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 2-->
+                                <img src="../imagens/user.png">
+                            </div>
+                        </div>
+                        
+                        <div class="content_faixa_exames"><!--content dos exames-->
+                            <div class="content_nome_exames"><!--nome exames-->
+                                <a>Cardiologia</a>
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 1-->
+                                <img src="../imagens/info.png">
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 2-->
+                                <img src="../imagens/user.png">
+                            </div>
+                        </div>
+                        
+                        <div class="content_faixa_exames"><!--content dos exames-->
+                            <div class="content_nome_exames"><!--nome exames-->
+                                <a>Cardiologia</a>
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 1-->
+                                <img src="../imagens/info.png">
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 2-->
+                                <img src="../imagens/user.png">
+                            </div>
+                        </div>
+                        
+                        <div class="content_faixa_exames"><!--content dos exames-->
+                            <div class="content_nome_exames"><!--nome exames-->
+                                <a>Cardiologia</a>
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 1-->
+                                <img src="../imagens/info.png">
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 2-->
+                                <img src="../imagens/user.png">
+                            </div>
+                        </div>
+                        
+                        <div class="content_faixa_exames"><!--content dos exames-->
+                            <div class="content_nome_exames"><!--nome exames-->
+                                <a>Cardiologia</a>
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 1-->
+                                <img src="../imagens/info.png">
+                            </div>
+
+                            <div class="acoes_exames"><!--ação 2-->
+                                <img src="../imagens/user.png">
                             </div>
                         </div>
 
+                    </div>
+                    
+                    <div class="content_descricao_procedimento">
+                        <div class="content_descricao">
+                            <div class="titulo_descricao">
+                                <a>Descrição</a>
+                            </div>
+
+                            <div class="conteudo_descricao">
+                                Revascularização miocárdica - pontes de safena: A cirurgia de pontes de safena é a cirurgia mais frequentemente realizada no IC-FUC. As artérias coronárias podem ficar obstruídas por placas de gordura que se formam em suas paredes. As pontes de safena passam por sobre estas obstruções, restabelecendo o fluxo de sangue nas áreas atingidas pela obstrução. A cirurgia envolve a sutura (costura) de uma veia (retirada da perna, ou de uma artéria retirada do tórax ou de outra parte do corpo) na aorta e distalmente a lesão obstrutiva da artéria, formando uma ponte que leva o sangue oxigenado ao músculo cardíaco.
+                                
+                            </div>
+                        </div>
+                        
+                        <div class="content_procedimento">
+                            <div class="titulo_procedimento">
+                                <a>Procedimento</a>
+                            </div>
+
+                            <div class="conteudo_procedimento">
+                                <a>Procedimentos híbridos: O futuro da cirurgia cardiovascular é a cirurgia minimamente invasiva, com pequenas incisões e sem o uso da CEC. Com a introdução dos stents para o tratamento das lesões das coronárias em casos selecionados, a cirurgia minimamente invasiva para a revascularização da artéria coronária descendente anterior usando a anastomose da artéria torácica interna é o tratamento ideal para estas lesões, e o tratamento com stents das lesões nos outros vasos se tornou uma alternativa válida à cirurgia convencional de revascularização do miocárdio. O tratamento com cateter das lesões estruturais do coração em cardiopatias congênitas e nas doenças das válvulas cardíacas diminuiu a mortalidade e aumentou a expectativa de vida dos pacientes. A terapia endovascular também se tornou o tratamento de escolha em alguns casos de doenças da aorta e artérias periféricas. A associação do tratamento cirúrgico com o tratamento endovascular é um dos grandes progressos na terapêutica cardiológica. Esta nova tecnologia exige salas especiais para sua realização e foi implantada no IC-FUC em fevereiro de 2011 com a inauguração de sua Sala Para Procedimentos Híbridos - a Sala Híbrida - junto ao Bloco Cirúrgico. </a>
+                            </div>
+                        </div>
+                        
+                        
                     </div>
                 </div>
 
