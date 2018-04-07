841d38e692dc7b0e1a15e00fdd81b7b008cf1055
littlelittlecy <cyntia.aparecida@hotmail.com>
2018-04-07 18:14:31 -0300

    sistema interno home

---
+++ sistema_interno/views/home.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Sistema Interno HHealth</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style_home.css">
+    </head>
+    <body>
+        <div id="principal">
+            <header>
+                SISTEMA INTERNO HHEALTH
+            </header>
+            <div class="main">
+                <div id="container_home">
+                    <div class="coluna">
+                        <img src="imagens/doc_icon1.jpg" alt="medicos" title="medicos">
+                        <p>Cadastrar funcionários</p>
+                        <p>Ver funcionários</p>
+                    </div>
+                    <div class="coluna">
+                        <img src="imagens/doc_icon2.jpg" alt="medicos" title="medicos">
+                        
+                        <p>Cadastrar pacientes</p>
+                        <p>Ver pendentes</p>
+                        <p>Ver pacientes ativos</p>
+                    </div>
+                    <div class="coluna">
+                        <img src="imagens/doc_icon3.jpg" alt="medicos" title="medicos">
+                        <p>Cadastrar níveis de usuário</p>
+                        <p>Ver níveis de usuário</p>
+                        <p>Cadastrar especialidades</p>
+                        <p>Ver especialidades</p>
+                    </div>
+                </div>
+            </div>
+            <footer>
+                Desenvolvido por CPI - 2018
+            </footer>
+        </div>
+    </body>
+</html>
